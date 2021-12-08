4160575eb75d038fcf119df2df59c1a422c68d72
Audrey Andoy <audreylandoy@gmail.com>
2021-12-07 23:04:43 -0800

    snowman so far

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ const lettersList = [
 let wins = 0;
 let losses = 0;
 let guesses = 9;
-let guessesList= [];
-let snowmanLetter = letterList[Math.floor(Math.random() * lettersList.length)];
+let guessesList = [];
+let snowmanLetter = lettersList[Math.floor(Math.random() * lettersList.length)];
 
 // const state = {
     //     wins: 0,
@@ -42,35 +42,45 @@ let snowmanLetter = letterList[Math.floor(Math.random() * lettersList.length)];
     
     // }
     
-    const playGame = (event) => {
+const playGame = (event) => {
     let playerLetter = event.key;
-    if (computerChoices.includes(playerLetter)) {
-        if (playerLetter === computerLetter) {
-          wins++;
+
+    if (lettersList.includes(playerLetter)) {
+        if (playerLetter === snowmanLetter && guesses < 9) {
+        //   wins++;
           guesses = 9;
           guessesList = [];
-          snowmanLetter = lettersList[Math.floor(Math.random() * computerChoices.length)];
+          document.querySelector("#snowman-letter").textContent = snowmanLetter;
           alert("You got it!");
-        } else if (playerLetter != computerLetter) {
-          alreadyGuessed.push(playerLetter)
+          snowmanLetter = lettersList[Math.floor(Math.random() * lettersList.length)];
+          document.removeEventListener("keydown", playGame);
+        } else if (playerLetter != snowmanLetter) {
+            guessesList.push(playerLetter)
           guesses--;
         }
         if (guesses === 0) {
-          losses++;
+        //   losses++;
           guesses = 9;
           guessesList = [];
-          snowmanLetter = lettersList[Math.floor(Math.random() * computerChoices.length)];
-          
+          snowmanLetter = lettersList[Math.floor(Math.random() * lettersList.length)];
+          document.removeEventListener("keydown", playGame);
+
         }
     } else {
         alert("You must choose a letter!");
     }
     
-    document.getElementById("player-guess").innerHTML = "   " + playerLetter;
+    document.getElementById("player-guess").innerHTML = "" + playerLetter;
     document.getElementById("win-counter").innerHTML = " " + wins;
     document.getElementById("loss-counter").innerHTML = " " + losses;
     document.getElementById("remaining").innerHTML = " " + guesses;
-    document.getElementById("guessed").innerHTML = " " + alreadyGuessed;
+    document.getElementById("guessed").innerHTML = "" + guessesList + ", ";
 }
-    
+
+// const registerHandlers = (event) => {
+    //     const start = document.querySelector("#startButton");
+    //     start.addEventListener("click", playGame);
+    // }
+
+
 document.addEventListener("keydown", playGame);
